7bdf7652b9abbba471a14f73d5813d478996a7ea
Akhil Gurjar <akhilgurjarji2508@gmail.com>
2025-07-25 14:34:55 +0530

    Add Jump and Run

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,8 +1,19 @@
+using Unity.Android.Gradle.Manifest;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    private float gravity = -9.81f;
+    private float jumpHeight = 3f;
+    private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundCheck;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float walkSpeed = 5f;
+    private float runSpeed = 10f;
+    private float currentSpeed;
     private CharacterController characterController;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,8 +27,26 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(horizontal, 0, vertical);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0 ) 
+        {
+            velocity.y = -2f;
+        }
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        Vector3 move = transform.right * horizontal + transform.forward * vertical;
 
-        characterController.Move(move * speed * Time.deltaTime);
+        characterController.Move(move * currentSpeed * Time.deltaTime + Vector3.up * velocity.y * Time.deltaTime);
     }
 }
