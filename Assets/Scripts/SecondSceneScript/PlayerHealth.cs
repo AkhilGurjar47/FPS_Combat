14e58b8a7e66ae4337be4094d3322584501599c6
Akhil Gurjar <akhilgurjarji2508@gmail.com>
2025-07-30 12:00:13 +0530

    Player Health Bar Script

--- Assets/Scripts/SecondSceneScript/PlayerHealth.cs
+++ Assets/Scripts/SecondSceneScript/PlayerHealth.cs
@@ -18,6 +18,51 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            TakeDamage(Random.Range(5,10));
+        }
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            RestoreHealth(Random.Range(5,10));
+        }
+    }
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if(fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            Debug.Log(lerpTimer);
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if(fillF < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+    }
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
     }
 }
